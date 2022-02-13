b5367538fdfcb3819e74ab202e9f2cbb110fbcc8
dantegr <tampantelis@gmail.com>
2022-02-13 15:06:40 +0100

    saving changes

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require("body-parser")
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-const routes = require('./endpoints/route.js')
-app.use('/', routes)
+const routes = require("./endpoints/route.js");
+app.use("/", routes);
 
 /* app.get('/', (req, res) => res.send('Hello World!')) */
 
-const PORT = 3001
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
